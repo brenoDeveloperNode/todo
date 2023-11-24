803aebf1d285038822acbe828bcda97aa7b3d77a
Tavilo Breno <tavilo.breno10@gmail.com>
2023-11-24 08:54:21 -0300

    Criando header e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-   <div>
-    TODO REACT
-   </div>
+    <div>
+      <Header />
+      <h1>Conteúdo</h1>
+      <Footer />
+    </div>
   );
 }
 
